c6fb68c13d06a313ae43c9d318e62b9f776c31f6
Evgen Savinov <evs@sitecore.net>
2017-05-12 17:05:17 +0300

    initial commit

---
+++ src/Sitecore.Support.7288/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Pipelines.Response.GetXmlBasedLayoutDefinition;
+using Sitecore.Mvc.Presentation;
+using Sitecore.XA.Feature.StickyNotes;
+using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+
+namespace Sitecore.Support.XA.Feature.StickyNotes.Pipelines.GetXmlBasedLayoutDefinition
+{
+    public class AddStickyNotesRendering:Sitecore.XA.Feature.StickyNotes.Pipelines.GetXmlBasedLayoutDefinition.AddStickyNotesRendering
+    {
+        public override void Process(GetXmlBasedLayoutDefinitionArgs args)
+        {
+            Item item = args.ContextItem ?? PageContext.Current.Item;
+            if (item == null)
+            {
+                return;
+            }
+            if (item.InheritsFrom(Templates._StickyNote.ID) && !Sitecore.Context.PageMode.IsExperienceEditor)
+            {
+                return;
+            }             
+            if (item.InheritsFrom(Templates._StickyNote.ID))
+            {
+                this.AddRendering(args, item);
+            }
+        }
+
+      
+    }
+}
---
+++ src/Sitecore.Support.7288/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.7288")]
+[assembly: AssemblyProduct("Sitecore.Support.7288")]
+[assembly: ComVisible(false)]
